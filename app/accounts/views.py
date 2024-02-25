85e669e964e6bdcf95d5f2c9b890e91ca4fb689f
Jose Felipe <felipe.brx.dev@gmail.com>
2024-02-25 15:18:37 -0300

    refactor accounts

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,15 +1,8 @@
-from django.views import View
-from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.http import JsonResponse
-
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 
 from app.accounts.serializers import SignInSerializer, AccountCreateSerializer, UserInfoSerializer
