ad9ddac395feb01056ce92a1c245ac016b985880
Jose Felipe <felipe.brx.dev@gmail.com>
2024-02-25 15:17:39 -0300

    refactor model review

--- app/reviews/models.py
+++ app/reviews/models.py
@@ -7,7 +7,7 @@ from app.common.models import BaseModel
 class Review(BaseModel):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     text = models.TextField(null=True, blank=True)
-    note = models.PositiveIntegerField(max_length=2, validators=[RegexValidator(r'\b([0-9]|10)\b')])
+    note = models.PositiveIntegerField(validators=[RegexValidator(r'\b([0-9]|10)\b')])
     tourist_spot = models.ForeignKey('touristic_points.TouristicPoint', on_delete=models.CASCADE)
 
     def __str__(self):
