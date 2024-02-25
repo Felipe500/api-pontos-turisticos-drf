08c805ed524e33652c2761bceb07b5536f820a5a
Jose Felipe <felipe.brx.dev@gmail.com>
2024-02-25 15:16:51 -0300

    add context view

--- app/common/view.py
+++ app/common/view.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 
 class ViewCommon(viewsets.ModelViewSet):
 
+    def get_serializer_context(self):
+        return {'request': self.request, 'user_id': self.request.user.id}
+
     def update(self, request, *args, **kwargs):
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
